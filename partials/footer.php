e67d469e5d26e19a7a709d7ec7810c06d9fe8732
Turbo <oelsayed314@gmail.com>
2026-05-03 03:49:12 +0400

    style: update Arabic heading font and social links
    - Change Arabic heading font from 'Handjet' to 'Noto Kufi Arabic' for better readability
    - Update Facebook and Instagram links to the user's specific profiles

--- partials/footer.php
+++ partials/footer.php
@@ -8,8 +8,8 @@
                     <p>&copy; <?= date('Y') ?> <?= t('hero_name') ?>. <?= t('footer_rights') ?></p>
                 </div>
                 <div class="footer-socials">
-                    <a href="https://instagram.com/omarelsayed" target="_blank" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
-                    <a href="https://facebook.com/omarelsayed" target="_blank" aria-label="Facebook"><i class="fab fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/omar_hesham_turbo/" target="_blank" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.facebook.com/omar.turboo.1/" target="_blank" aria-label="Facebook"><i class="fab fa-facebook"></i></a>
                     <a href="https://<?= translate('contact_github') ?>" target="_blank" aria-label="GitHub"><i class="fab fa-github"></i></a>
                     <a href="https://<?= translate('contact_linkedin') ?>" target="_blank" aria-label="LinkedIn"><i class="fab fa-linkedin"></i></a>
 
